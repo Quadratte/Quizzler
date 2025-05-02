ffe17db1ae61f71f0b1598243db0c44aee95cc92
Sergey Philippov <Quadratte@gmail.com>
2025-05-02 17:40:02 +0700

    add quiz logic

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -1,8 +1,10 @@
-
 import UIKit
 
 class ViewController: UIViewController {
-    
+  
+  var subScript = 0
+  let arrayOfQuestions = ["huy", "pizda", "dgigurda"]
+  
   @IBOutlet weak var questionText: UILabel!
   @IBOutlet weak var trueButton: UIButton!
   @IBOutlet weak var falseButton: UIButton!
@@ -11,9 +13,21 @@ class ViewController: UIViewController {
   
   override func viewDidLoad() {
         super.viewDidLoad()
-       
+    questionText.text = "Why are you gae?"
     }
 
+  @IBAction func answerButtonTapped(_ sender: Any) {
+    if arrayOfQuestions.count-2 >= subScript {
+      subScript += 1
+      print(subScript)
+      questionText.text = arrayOfQuestions[subScript]
+    } else {
+      subScript = 0
+      questionText.text = arrayOfQuestions[subScript]
+    }
 
+  }
+  
 }
+
 
